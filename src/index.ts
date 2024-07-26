03925c4ea7400b2c8a69f06890b9255d7f1f67e3
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-26 16:13:18 +0800

    feat: Add implementation to NoteRepository

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
+import { PostgresConnectionFactory } from "tsdbc-pg/postgres";
+import { PostgresNoteRepository } from "./note-repository";
+import { NoteService, NoteServiceImplementation } from "./note-service";
+import { HttpError } from "./http-errors";
 
 type Bindings = {
   DATABASE_URL: string;
@@ -6,8 +10,36 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/", (context) => {
-  return context.json({ message: "Hello, World" });
+const createService = (databaseURL: string): NoteService => {
+  const connection = PostgresConnectionFactory.create(databaseURL);
+  const repository = new PostgresNoteRepository(connection);
+  return new NoteServiceImplementation(repository);
+};
+
+app.get("/notes", async (context) => {
+  const databaseURL = context.env.DATABASE_URL;
+  const service = createService(databaseURL);
+  return context.json(await service.findAll());
+});
+
+app.get("/notes/:id", async (context) => {
+  const databaseURL = context.env.DATABASE_URL;
+  const service = createService(databaseURL);
+  const id = Number(context.req.param("id"));
+  return context.json(await service.findById(id));
+});
+
+app.delete("/notes/:id", async (context) => {
+  const databaseURL = context.env.DATABASE_URL;
+  const service = createService(databaseURL);
+  const id = Number(context.req.param("id"));
+  return context.json(await service.deleteById(id));
+});
+
+app.onError((error: Error, context: Context) => {
+  if (error instanceof HttpError) return error.handle(context);
+  context.status(500);
+  return context.json({ message: "server error" });
 });
 
 export default app;
--- src/note-repository.ts
+++ src/note-repository.ts
@@ -1,5 +1,6 @@
 import { CanRead, CanDelete, CanCheckExistence } from "./interfaces";
-import { Note } from "./note";
+import { Note, NoteBuilder } from "./note";
+import { DatabaseConnection, RowData } from "tsdbc-pg";
 
 export interface NoteRepository
   extends CanRead<number, Note>,
@@ -7,4 +8,72 @@ export interface NoteRepository
     CanCheckExistence<number> {
   findByTitle(title: string): Promise<Note>;
   existByTitle(title: string): Promise<boolean>;
+}
+
+export class PostgresNoteRepository implements NoteRepository {
+  constructor(private connection: DatabaseConnection) {}
+
+  private static buildNote(row?: RowData) {
+    return new NoteBuilder()
+      .id(row?.getNumber("id") as number)
+      .title(row?.getString("title") as string)
+      .body(row?.getString("body") as string)
+      .timeStamp(row?.getDate("time_stamp") as Date)
+      .build();
+  }
+
+  async findByTitle(title: string): Promise<Note> {
+    const rows = await this.connection
+      .prepareStatement("SELECT * FROM notes WHERE title = ?")
+      .setString(title)
+      .execute();
+    const row = rows.at(0);
+    return PostgresNoteRepository.buildNote(row);
+  }
+
+  async existByTitle(title: string): Promise<boolean> {
+    const rows = await this.connection
+      .prepareStatement('SELECT count(id) as "count" WHERE title = ?')
+      .setString(title)
+      .execute();
+    const row = rows.at(0);
+    const count = row?.getNumber("count") as number;
+    return count > 0;
+  }
+
+  async findById(id: number): Promise<Note> {
+    const rows = await this.connection
+      .prepareStatement("SELECT * FROM notes WHERE id = ?")
+      .setNumber(id)
+      .execute();
+    const row = rows.at(0);
+    return PostgresNoteRepository.buildNote(row);
+  }
+
+  async findAll(): Promise<Note[]> {
+    return (
+      await this.connection
+        .createStatement("SELECT * FROM notes ORDER BY time_stamp DESC")
+        .execute()
+    ).map((row) => PostgresNoteRepository.buildNote(row));
+  }
+
+  async deleteById(id: number): Promise<Note> {
+    const note = await this.findById(id);
+    return await this.connection
+      .prepareStatement("DELETE FROM notes WHERE id = ?")
+      .setNumber(id)
+      .execute()
+      .then(() => note);
+  }
+
+  async existById(id: number): Promise<boolean> {
+    const rows = await this.connection
+      .prepareStatement("SELECT count(id) FROM notes WHERE id = ?")
+      .setNumber(id)
+      .execute();
+    const row = rows.at(0);
+    const count = row?.getNumber("count") as number;
+    return count > 0;
+  }
 }
